73e9ed6682f3fb75ec1986c1cbcf45b75dfedb38
captainvlad <zadorozhnyy@ucu.edu.ua>
2019-12-24 18:00:50 +0200

    First version

---
+++ light_mode/Debug/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_ctlreq.d
@@ -0,0 +1,122 @@
+Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_ctlreq.o: \
+ ../Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_ctlreq.c \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ctlreq.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_core.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Inc/usbh_conf.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Inc/main.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Inc/stm32f4xx_hal_conf.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/CMSIS/Include/core_cm4.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/CMSIS/Include/cmsis_version.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/CMSIS/Include/mpu_armv7.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_def.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ioreq.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_pipes.h \
+ /home/vlad/Desktop/2_year/POK/project/light_mode/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ctlreq.h
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ctlreq.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_core.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Inc/usbh_conf.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Inc/main.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Inc/stm32f4xx_hal_conf.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/CMSIS/Include/core_cm4.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/CMSIS/Include/cmsis_version.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/CMSIS/Include/mpu_armv7.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_def.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ioreq.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_pipes.h:
+
+/home/vlad/Desktop/2_year/POK/project/light_mode/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ctlreq.h:
